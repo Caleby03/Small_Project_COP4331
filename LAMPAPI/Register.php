38c974aed0891a30a173d1d8251abdabcabfdd42
Caleb Yaghoubi <Caleby315@gmail.com>
2025-08-28 02:33:35 +0000

    Updated Register.php to check for if new user login is the same as another login

--- LAMPAPI/Register.php
+++ LAMPAPI/Register.php
@@ -14,8 +14,8 @@
 	}
 	else
 	{
-        $stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE FirstName=? AND LastName =?");
-		$stmt->bind_param("ss", $inData["firstName"], $inData["lastName"]);
+        $stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=?");
+		$stmt->bind_param("s", $inData["login"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
