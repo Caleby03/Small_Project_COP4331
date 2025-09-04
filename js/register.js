e2d699d77fb416f4b677ea8c4027342f1d16e271
Caleb Yaghoubi <Caleby315@gmail.com>
2025-09-03 22:36:16 -0400

    updated to not jump back to login.html

--- js/register.js
+++ js/register.js
@@ -35,7 +35,6 @@ document.addEventListener('DOMContentLoaded', () => {
                         return;
                     }
                     errorTxt.innerHTML = "Account Created!";
-                    window.location.href = "./login.html";
                 }
             };
             xhr.send(jsonPayload);
