0914e809f531049cd2557f9063a1023d7ffc1f15
Ameer Khalil <ameerkhalil795@gmail.com>
2025-09-08 17:58:45 -0400

    Add munish delete.php file

--- LAMPAPI/delete.php
+++ LAMPAPI/delete.php
@@ -2,53 +2,53 @@
 	$inData = getRequestInfo();
 	$userId = $inData["userId"];
 	$contactId=$inData["contactId"];
-	$conn = new mysqli("localhost", "UnNamed", "Small", "COP4331", 3306); 	
+	$conn = new mysqli("localhost", "UnNamed", "Small", "COP4331", 3306);
 
-	if ($conn->connect_error) 
+	if ($conn->connect_error)
 
 	{
 
 		returnWithError( $conn->connect_error );
 
-	} 
+	}
 
 	else
 	{
 		$stmt = $conn->prepare("SELECT COUNT(*) FROM Contacts WHERE UserID=? AND ID=?");
-		$stmt->bind_param("ii",$userId,$contactId); 
-		$stmt->execute(); 
-		$result=$stmt->get_result(); 
-		$row=$result->fetch_row(); 
+		$stmt->bind_param("ii",$userId,$contactId);
+		$stmt->execute();
+		$result=$stmt->get_result();
+		$row=$result->fetch_row();
 
 		if($row[0]==0)
 		{
-			returnWithError( "User was not found, therefore they were not deleted."); 
+			returnWithError( "User was not found, therefore they were not deleted.");
 		}
 		else
 		{
-			$stmt->close(); 
-			$stmt=$conn->prepare("DELETE FROM Contacts WHERE UserID=? AND ID=?"); 
+			$stmt->close();
+			$stmt=$conn->prepare("DELETE FROM Contacts WHERE UserID=? AND ID=?");
 			$stmt->bind_param("ii",$userId,$contactId);
-			$stmt->execute(); 
-				
+			$stmt->execute();
+
 			if($stmt->affected_rows==1)
 			{
-				returnWithError(""); 
+				returnWithError("");
 			}
 			else
 			{
-				returnWithError("An error occurred. Please try again."); 
-			} 
+				returnWithError("An error occurred. Please try again.");
+			}
 
 		}
-		$stmt->close(); 
-		$conn->close(); 
+		$stmt->close();
+		$conn->close();
 
 	}
 
 	function getRequestInfo()
 	{
-		return json_decode(file_get_contents('php://input'),true); 
+		return json_decode(file_get_contents('php://input'),true);
 	}
 
 
