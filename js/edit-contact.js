310affeb11073bc006a37b10ccc21ef8334ed3a8
Ameer Khalil <ameerkhalil795@gmail.com>
2025-09-09 02:42:09 -0400

    Testing update contact 5

--- js/edit-contact.js
+++ js/edit-contact.js
@@ -9,13 +9,18 @@ const contactId = new URLSearchParams(window.location.search).get("id");
 
 function loadContact(id){
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", urlbase + '/update.' + extension, true);
+    xhr.open("POST", urlbase + '/SearchContacts.' + extension, true);
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
 
     xhr.onreadystatechange = function () {
         if(xhr.readyState === 4 && xhr.status === 200){
             try{
                 const response = JSON.parse(xhr.responseText);
+                if(response.error){
+                    console.error("Error:", response.error);
+                    return;
+                }
+
                 console.log(response.results);
             }
             catch(error){
@@ -23,7 +28,7 @@ function loadContact(id){
             }
         }
     }
-    const payload = JSON.stringify({search: "", userId: Number(sessionStorage.getItem("userId"))});
+    const payload = JSON.stringify({userId: Number(sessionStorage.getItem("userId"))});
     xhr.send(payload);
 }
 
